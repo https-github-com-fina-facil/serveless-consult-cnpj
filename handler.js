6e2ab51b3339604033016f067150fe4739e0630a
fina-facil <contato@finafacil.com>
2021-06-28 11:01:57 -0300

    Hotfix: Ajustes serveless.

--- handler.js
+++ handler.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const pdf = require('html-pdf')
 const handler = require('serverless-express/handler')
 const express = require('serverless-express/express')
 
@@ -9,11 +10,50 @@ app.use(express.urlencoded({extended: true}))
 app.post('/api/consulta-cnpj-api', async (req, res) => {
   let {cnpj, token, url} = req.body
 
-  let {data} = await axios.get(`${url}?token=${token}&cnpj=${cnpj}&format=json`)
+  console.log('CNPJ', cnpj)
+  console.log('TOKEN', token)
+  console.log('URL', url)
+  try {
+    let {data} = await axios.get(`${url}?token=${token}&cnpj=${cnpj}&format=json`)
 
-  console.log('Res', data)
+    return res.json(data)
+  } catch (error) {
+    console.error(error)
+  }
+})
 
-  return res.json(data)
+app.get('/api/api-servless-fina', async (req, res) => {
+  try {
+    return res.json({message: 'Api Fina Fácil!'})
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+app.post('/api/download-files', async (req, res) => {
+  let {html, fileNamePDF} = req.body
+
+  let data = new Promise(async (resolve, reject) => {
+    const blobSvc = azure.createBlobService(CONNECTION_STRING)
+    var writable = fs.createWriteStream(filename)
+    blobSvc.getBlobToStream(CONTAINER, filename, writable, (error, result) => {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log(result)
+      }
+
+      fs.readFile(filename, function (err, data) {
+        if (err) {
+          console.error(err)
+        }
+
+        resolve(data)
+      })
+    })
+  })
+
+  console.log('File', data)
 })
 
 exports.handler = handler(app)
